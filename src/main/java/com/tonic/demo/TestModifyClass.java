4f5a6e46db73ba994abb2a3ece2b6e37bd361ec7
TonicBox <gsec.tonicbox@protonmail.com>
2025-01-21 23:33:20 -0600

    Moved demos to demo package. Implimented Byttecode class and other QoL builder classes.

--- src/main/java/com/tonic/demo/TestModifyClass.java
+++ src/main/java/com/tonic/demo/TestModifyClass.java
@@ -1,7 +1,6 @@
-package com.tonic;
+package com.tonic.demo;
 
 import com.tonic.analysis.Bytecode;
-import com.tonic.analysis.instruction.ReturnInstruction;
 import com.tonic.parser.ClassFile;
 import com.tonic.parser.ClassPool;
 import com.tonic.parser.ConstPool;
@@ -14,38 +13,33 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
-public class Main {
+public class TestModifyClass
+{
     public static void main(String[] args) throws IOException {
-        // Disable logger output (optional)
         Logger.setLog(false);
-
-        // Acquire our default ClassPool instance
         ClassPool classPool = ClassPool.getDefault();
-
-        // Use a try-with-resources to ensure the InputStream is properly closed
-        try (InputStream is = Main.class.getResourceAsStream("TestCase.class")) {
+        try (InputStream is = TestClassCreation.class.getResourceAsStream("TestCase.class")) {
             if (is == null) {
                 throw new IOException("Resource 'TestCase.class' not found.");
             }
-            ClassFile classFile = classPool.loadClass(is);
-            classFile.createNewMethod(0x0001, "testVoid", void.class);
-            classFile.createNewMethod(0x0001, "testInt", int.class);
-            classFile.createNewMethod(0x0001, "testObjWithParams", Object.class, int.class, String.class);
 
-            int access = new AccessBuilder().setPublic().build();
+            ClassFile classFile = classPool.loadClass(is);
+            classFile.setClassName("com.tonic.TestCaseModified");
+
+            int access = new AccessBuilder().setPublic().setStatic().build();
+
             classFile.createNewField(access, "testIntField", "I", new ArrayList<>());
             MethodEntry method = classFile.createNewMethod(false, access, "demoGetter", int.class);
             Bytecode bytecode = new Bytecode(method);
 
             ConstPool constPool = bytecode.getConstPool();
             int fieldRefIndex = constPool.findOrAddField("com/tonic/TestCase", "testIntField", "I");
-            bytecode.addALoad(0);
-            bytecode.addGetField(fieldRefIndex);
+            bytecode.addGetStatic(fieldRefIndex);
             bytecode.addReturn(ReturnType.IRETURN); // IRETURN opcode
             bytecode.finalizeBytecode();
 
             classFile.rebuild();
-            System.out.println(classPool.get("com.tonic.TestCase"));
+            System.out.println(classPool.get("com.tonic.TestCaseModified"));
         }
     }
 }
